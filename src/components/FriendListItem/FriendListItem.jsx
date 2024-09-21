7160b4d4699d60eab9acd0652f5d7184679f2003
kiryhanchiiiik <kirillsabelkijk@gmail.com>
2024-09-21 20:28:59 +0300

    changed styles

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,9 +1,10 @@
+import styles from './FriendListItem.module.css';
 const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <div>
       <img src={avatar} alt="Avatar" width="48" />
       <p>{name}</p>
-      <p style={{ color: isOnline ? 'green' : 'red' }}>
+      <p className={isOnline ? styles.statusOnline : styles.statusOffline}>
         {isOnline ? 'Online' : 'Offline'}
       </p>
     </div>
